4b7eab1a174319e03913578c5d749fa8b01d851c
Gonzalo Gamez <takosaga@gmail.com>
2023-10-29 12:37:16 +0200

    feat: Chapter 1 work
    feat: practice questions added
    
    feat: add tabs and 2 answers
    
    feat: add 3 answers
    
    feat: finished chapter 1

--- chapter_1.py
+++ chapter_1.py
@@ -1,8 +1,74 @@
 # 1 Which of the following are operators and which are values?
 """
+Practice Questions
+    1. Which of the following are operators, and which are values?
 
+*
+'hello'
+-88.8
+-
+/
++
+5
 
+    A1.
+*, -, +, 5 are operators
 
+    2. Which of the following is a variable, and which is a string?
+
+spam
+'spam'
+
+    A2.
+spam is variable and 'spam' is a string
+
+    3. Name three data types.
+
+    A3.
+int, str, float, bool
+
+    4. What is an expression made up of? What do all expressions do?
+
+    A4.
+Expressions are made of values and operators where they can be evaluated
+
+    5. This chapter introduced assignment statements, like spam = 10. What is the difference between an expression and a statement?
+
+    A5.
+An expression is evaulated and a statement is assignment to a value
+
+    6. What does the variable bacon contain after the following code runs?
+
+bacon = 20
+bacon + 1
+
+    A6:
+20
+
+    7. What should the following two expressions evaluate to?
+
+'spam' + 'spamspam'
+'spam' * 3
+
+    A7.
+'spamspamspam' and 'spamspamspam'
+
+    8. Why is eggs a valid variable name while 100 is invalid?
+
+    A8.
+egg follows variable names while 100 starts with number
+
+    9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+
+    A9.
+int(), float(), str()
+
+    10. Why does this expression cause an error? How can you fix it?
+
+'I have eaten ' + 99 + ' burritos.'
+
+    A10.
+You can not concantenate str and int. Fixed by changing 99 to str(99) to read as a string
 
 
 """
